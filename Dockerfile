de2c0e30ff646407adcb37d042d7730d3b64bb3c
Tom Black <tom@tcpip.uk>
2023-07-10 22:01:42 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use an official Python runtime as a parent image
+FROM python:3.8-slim-buster
+
+# Set work directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+ADD . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir chatgpt-memory
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+from chatgpt_memory.environment import OPENAI_API_KEY, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT
+from chatgpt_memory.datastore.config import RedisDataStoreConfig
+from chatgpt_memory.datastore.redis import RedisDataStore
+from chatgpt_memory.llm_client.openai.conversation.chatgpt_client import ChatGPTClient
+from chatgpt_memory.llm_client.openai.conversation.config import ChatGPTConfig
+from chatgpt_memory.llm_client.openai.embedding.config import EmbeddingConfig
+from chatgpt_memory.llm_client.openai.embedding.embedding_client import EmbeddingClient
+from chatgpt_memory.memory.manager import MemoryManager
+
+# Get environment variables
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
+# Create necessary objects
+embedding_config = EmbeddingConfig(api_key=OPENAI_API_KEY)
+embed_client = EmbeddingClient(config=embedding_config)
+
+redis_datastore_config = RedisDataStoreConfig(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+)
+redis_datastore = RedisDataStore(config=redis_datastore_config)
+redis_datastore.connect()
+redis_datastore.create_index()
+
+memory_manager = MemoryManager(datastore=redis_datastore, embed_client=embed_client, topk=1)
+
+chat_gpt_client = ChatGPTClient(
+    config=ChatGPTConfig(api_key=OPENAI_API_KEY, verbose=True), 
+    memory_manager=memory_manager
+)
+
+# Interact with the ChatGPT client
+conversation_id = None
+while True:
+    user_message = input("\n Please enter your message: ")
+    response = chat_gpt_client.converse(message=user_message, conversation_id=conversation_id)
+    conversation_id = response.conversation_id
+    print(response.chat_gpt_answer)
